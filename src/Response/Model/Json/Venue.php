538d2fd33614390e335a73bd96b8cb3bdd9bbc29
Guillaume Le Nouvel <guillaume.lenouvel@gmail.com>
2021-06-18 10:20:10 +0200

    Add mapping venue - add test venue'

---
+++ src/Response/Model/Json/Venue.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Response\Model\Json;
+
+use Soundcharts\ApiClientBundle\Response\Model\Json\AbstractResponse;
+
+class Venue extends AbstractResponse
+{
+    /**
+     * {@inheritDoc}
+     * @see \Soundcharts\ApiClientBundle\Response\Model\AbstractResponse::getMapping()
+     */
+    public function getMapping(): array
+    {
+        return [
+            'id'            => 'id',
+            'name'          => 'name',
+            'streetAddress' => 'streetAddress',
+            'city'          => 'city',
+            'region'        => 'region',
+            'countryCode'   => 'countryCode',
+            'postalCode'    => 'postalCode',
+            'capacity'      => 'capacity',
+            'popularity'    => 'popularity',
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return $this->getValue('id');
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->getValue('name');
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetAddress(): string
+    {
+        return $this->getValue('streetAddress');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->getValue('city');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegion(): string
+    {
+        return $this->getValue('region');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode(): string
+    {
+        return $this->getValue('countryCode');
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->getValue('postalCode');
+    }
+
+    /**
+     * @return int
+     */
+    public function getCapacity(): int
+    {
+        return $this->getValue('capacity');
+    }
+
+    /**
+     * @return float
+     */
+    public function getPopularity(): float
+    {
+        return $this->getValue('popularity', false);
+    }
+}
--- tests/Provider/SongkickProviderTest.php
+++ tests/Provider/SongkickProviderTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Provider;
 use App\Provider\SongkickProvider;
 use App\Response\Model\Json\Event;
 use App\Response\Model\Json\EventCollection;
+use App\Response\Model\Json\Venue;
 use App\Tests\ResponseMockTrait;
 use GuzzleHttp\ClientInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -50,5 +51,37 @@ class SongkickProviderTest extends KernelTestCase
         }
 
         $this->assertCount(50, $eventCollection);
+    }
+
+    public function testGetVenue()
+    {
+        self::bootKernel();
+
+        $client = $this->createMock(ClientInterface::class);
+
+        $client
+            ->expects($this->once())
+            ->method('request')
+            ->willReturn($this->getResponseMock(__DIR__ . '/../dataset/venue.json'));
+
+        $provider = new SongkickProvider($client,
+            self::$kernel->getContainer()->get('Soundcharts\ApiClientBundle\Response\MusicResponseBuilder'));
+
+        $venue = $provider->getVenue('test');
+
+        $this->assertInstanceOf(Venue::class, $venue);
+
+        $this->assertNotEmpty($venue);
+
+
+        $this->assertEquals('test', $venue->getIdentifier());
+        $this->assertEquals("Fiddler's Green Amphitheatre", $venue->getName());
+        $this->assertEquals('6350 Greenwood Plaza Blvd.', $venue->getStreetAddress());
+        $this->assertEquals('Greenwood Village', $venue->getCity());
+        $this->assertEquals('CO', $venue->getRegion());
+        $this->assertEquals('80111', $venue->getPostalCode());
+        $this->assertEquals('US', $venue->getCountryCode());
+        $this->assertEquals(18000, $venue->getCapacity());
+        $this->assertEquals(2.0, $venue->getPopularity());
     }
 }
